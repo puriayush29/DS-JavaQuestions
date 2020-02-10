e23b96aba7f6f7bc36bcfce9ec53e035cbe147c0
Ayush Puri <38379529+puriayush29@users.noreply.github.com>
2020-02-10 22:48:49 +0530

    Update LeetCodeCandyProblem.java

--- LeetCodeCandyProblem.java
+++ LeetCodeCandyProblem.java
@@ -1,73 +1,55 @@
 class Solution {
     public int candy(int[] ratings) {
         int candies[] = new int[ratings.length];
-        int sum =0,flag=0;
-       
-        for(int j=0;j<ratings.length;j++)
+        int sum=0;
+        for(int i=0;i<ratings.length;i++)
         {
-            candies[j] = 1;
+            candies[i]=1;
         }
         for(int i=1;i<ratings.length;i++)
         {
-            
-            if(ratings[i-1]> ratings[i])
-            {
-                candies[i-1]++;
-                if(i>1){
-                if(candies[i-1]==candies[i-2])
-                {
-                    if(ratings[i-2] > ratings[i-1])
-                    {
-                        candies[i-2]++;
-                    }
-                    else
-                    {
-                        candies[i-1]++;
-                    }
-                
-                }
-                    
-                    
-                }
-            }
-            else if(ratings[i] > ratings[i-1])
+            if(ratings[i-1] < ratings[i])
             {
                 candies[i]++;
-               
-                if(candies[i] == candies[i-1])
+                if(candies[i-1] == candies[i])
                 {
-                    if(ratings[i]>ratings[i-1])
-                    {
-                        candies[i]++;
-                    }
-                    else
-                    {
-                        candies[i-1]++;
-                    }
+                    candies[i]++;
                 }
-                else if(candies[i-1] > candies[i])
+                else if(candies[i] < candies[i-1])
                 {
-                    if(ratings[i]>ratings[i-1])
-                    {
-                        int n1 = candies[i-1]-candies[i];
-                        candies[i] = candies[i] + 1 + n1;
-                        
-                    }
+                    int n1 = candies[i-1] - candies[i];
+                    candies[i] = candies[i]+1+n1;
                 }
             }
-            if(i<ratings.length-1){
-            if(ratings[i-1] < ratings[i] && ratings[i] > ratings[i+1])
+            if(i!=ratings.length-1){
+            if(ratings[i-1] < ratings[i] && ratings[i+1] < ratings[i])
             {
                 candies[i]--;
             }
             }
         }
-       
-            for(int i=0;i<ratings.length;i++)
+        for(int i=ratings.length-1;i>=1;i--)
+        {
+            if(ratings[i-1]>ratings[i])
             {
-                sum+=candies[i];
+                candies[i-1]++;
+                if(candies[i-1] == candies[i])
+                {
+                    candies[i-1]++;
+                }
+                else if(candies[i-1]<candies[i])
+                {
+                    int n1 = candies[i] - candies[i-1];
+                    candies[i-1] = candies[i-1]+n1+1;
+                }
             }
             
-           return sum;
+            
+        }
+        for(int i=0;i<ratings.length;i++)
+        {
+            sum+=candies[i];
+        }
+        return sum;
     }
 }
